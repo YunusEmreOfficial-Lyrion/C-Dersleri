0a998055b8f85b9c5d54e10423ded1b4b325d123
YunusEmreOfficial <78508682+YunusEmreOfficial-Lyrion@users.noreply.github.com>
2024-12-22 23:54:07 +0300

    Add files via upload

---
+++ Fordöngüleri/src/Fordöngüleri.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	/*
+	              FOR KULLANIMI
+
+	   int = 1;
+
+	   for(i = 0; koşul ; i +=2){
+
+	   yapılacak kodlar
+	  }
+
+	  */
+
+	int i ;
+
+	for(i = 1; i <= 10; i++ ){
+
+		cout << "i: " << i << endl;
+
+	}
+
+
+	return 0;
+}
