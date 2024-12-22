d317fdacee3613b1d89e055a44eca77f440c5493
YunusEmreOfficial <78508682+YunusEmreOfficial-Lyrion@users.noreply.github.com>
2024-12-22 23:49:51 +0300

    Add files via upload

---
+++ Ders1/src/Ders1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	cout << "naber la tirrek" ;
+
+
+	return 0;
+}
---
+++ Ders2/src/Ders2.cpp
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : Ders2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Değişken Tipleri ve Değişkenler
+	/*	int
+	 * 	float
+	 * 	double
+	 * 	char
+	 * 	bool
+	 */
+	/*
+	int a = 32;
+	float b = 3.52;
+	double c = 4.546;
+	char d = 'A';
+	bool e = true;
+
+	cout << "A değişkeninin değeri : " << a << endl;
+	cout << "B değişkeninin değeri : " << b << endl;
+	cout << "C değişkeninin değeri : " << c << endl;
+	cout << "D değişkeninin değeri : " << d << endl;
+	cout << "E değişkeninin değeri : " << e << endl;
+
+	 */
+
+
+
+	int a,b,c,toplam = 0;
+
+	a = 4 ;
+	b = 3 ;
+	c = 5 ;
+
+	toplam = a + b + c ;
+
+	cout << " Toplam : " << toplam << endl;
+
+
+	return 0;
+}
