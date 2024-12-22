3da4cb724d800aa34bc39756ab7ce3f55038d054
YunusEmreOfficial <78508682+YunusEmreOfficial-Lyrion@users.noreply.github.com>
2024-12-22 23:50:47 +0300

    Add files via upload

---
+++ Ders3/src/Ders3.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : Ders3.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	/*
+
+	Matematik Operatörler
+
+		+, -, *, /
+
+	Artırma Operatörü
+	Azaltma Operatörü
+
+	*/
+	/*
+
+	int sayi1 = 10 ;
+	int sayi2 = 4 ;
+
+	cout << "Sayi1 + Sayi2 : " << sayi1 + sayi2 << endl;
+	cout << "Sayi1 - Sayi2 : " << sayi1 - sayi2 << endl;
+	cout << "Sayi1 * Sayi2 : " << sayi1 * sayi2 << endl;
+	cout << "Sayi1 / Sayi2 : " << float(sayi1) / sayi2 << endl;
+	*/
+
+	int a = 5;
+
+	cout << "a'nın değeri : " << a << endl;
+
+	//a = a + 2 ;
+	a += 2;
+
+	cout << "A'nın yeni değeri : " << a << endl;
+
+	return 0;
+}
---
+++ Ders4/src/Ders4.cpp
@@ -0,0 +1,57 @@
+//============================================================================
+// Name        : Ders4.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+/*
+Örnek 1 :
+
+	int x;
+
+	cout << "Bir sayı giriniz : " ;
+
+	cin >> x ;
+
+	cout << "Girdiğiniz sayı : " << x;
+
+
+	*/
+
+/*
+Örnek 2 :
+
+	int a, b, c;
+
+	cout << "Birinci sayiyi giriniz: " ;
+	cin >> a;
+
+	cout << "İkinci sayiyi giriniz: " ;
+	cin >> b;
+
+	cout << "Üçüncü sayiyi giriniz: " ;
+	cin >> c;
+
+	cout << "Girdiğiniz sayılar: " << a  << b << c << endl ;
+	cout << "Girdiğiniz sayıların toplamı : " << a + b + c;
+*/
+
+/*
+Örnek 3 :
+	*/
+	int a,b,c;
+
+	cout << "Sayıları giriniz: ";
+	cin >> a >> b >> c ;
+
+	cout << "Sayıların toplamı : " << a + b + c << endl;
+
+	return 0;
+}
