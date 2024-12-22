fd8c22e508562276e8937c9efb94fb9c4e53e406
YunusEmreOfficial <78508682+YunusEmreOfficial-Lyrion@users.noreply.github.com>
2024-12-23 00:01:01 +0300

    Add files via upload

---
+++ WhileDöngüleri/src/WhileDöngüleri.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int i = 0;
+
+	while (i <= 10){
+		cout << "i : " << i << endl;
+		i++; // i = i + 1; Şeklindede kullanabilirsin.
+	}
+
+
+	return 0;
+}
